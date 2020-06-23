02bd2b1499f8b54c4a44613733387a10ccf24cc5
Urchmaney <kingsobino@gmail.com>
2020-06-23 15:35:09 +0000

    Add retailerservice test

---
+++ Market.Tests/Database Service/RetailerService.cs
@@ -0,0 +1,15 @@
+using System;
+using Market.DataService.SQL;
+using Market.Interfaces.DataService;
+using Microsoft.EntityFrameworkCore;
+
+namespace Market.Tests.DatabaseService
+{
+  //private IRetailerService _retailerService;
+  public class RetailerService
+  {
+    // var option = new DbContextOptionBuilder<MarketDbContext>().UseInMemoryDatabase(databaseName: "Market").options;
+    // var marketContext = new MarketDbContext(option);
+    // _retailerService = new RetailerService(marketContext);
+  }
+}
